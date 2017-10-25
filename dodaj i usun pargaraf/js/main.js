4adb1d41d5f63c6f002134cb9067d627211723f7
Lukasz Marcinek <marcinek.lukasz@gmail.com>
2017-10-25 20:14:03 +0200

    moje zadanie

---
+++ dodaj i usun pargaraf/js/main.js
@@ -0,0 +1,9 @@
+
+$('#przyklad').text('asdasdas');
+$('button').addClass('link');
+$('button').click(function()
+                  {
+    alert('Jesteś najlpeszym koderem jakiego znam :)')
+});
+
+    
